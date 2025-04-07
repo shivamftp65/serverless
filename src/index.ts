c1007610a79491e1c26e811dc7498712534c968e
shivamftp65 <shivamftp65@gmail.com>
2025-04-08 02:50:05 +0530

    just a bit of messaging

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,17 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 } satisfies ExportedHandler<Env>;
